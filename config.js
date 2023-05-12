f4cdc7d69760de02b7bc2fa77f6a30f4de342299
Raingor <1017503523@qq.com>
2023-05-12 14:26:21 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    titleWeb: "Hello Tingting",
+    titleWeb: "老婆叽叽，爱妳",
     introTitle: '婷婷',
     introDesc: `地球很奇怪
     为什么我总是走错路？
@@ -8,13 +8,13 @@ const CONFIG = {
     btnIntro: '哈咯哈咯',
     title: '你是否一见倾心？🥰',
     desc: '是不是看到那个笑容就醉了？',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    btnYes: '爱妳~ 😘',
+    btnNo: '打咩 :3',
+    question: '世界上有超过70亿人，但你为什么爱我？ <3',
+    btnReply: '寄给你<3',
+    reply: '爱就爱，不爱就爱 <33333333',
+    mess: '我知道🥰. 就是这样爱妳~ 😘😘',
+    messDesc: '次数+1.',
+    btnAccept: '非常接受 <3',
+    messLink: 'https://raingor.github.io/loveAli/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
